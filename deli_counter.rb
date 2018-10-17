8a0a46039815135dc11fb2591f82724d1637b8c0
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-17 14:35:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,12 @@ def take_a_number(array, name)
   array << name
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    name = array.shift
+    puts "Currently serving #{name}."
+  end
+end
