763fcae47f8cc950b821abf1595015f5ae722d7e
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-17 14:29:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    string = "The line is currently: "
+    string = "The line is currently:"
     array.each_with_index do |name, index|
       string << " #{index + 1}. #{name}"
     end
